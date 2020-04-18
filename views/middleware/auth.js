99932167698795c5ca179009636e9e4a51713cf4
cagdas <cagdasmuldur@gmail.com>
2020-04-18 12:31:49 -0400

    fix

--- views/middleware/auth.js
+++
@@ -1,5 +0,0 @@
-const isLoggedIn = (req, res, next) => {
-  req.session.userInfo ? next() : res.redirect("/login");
-};
-
-module.exports = isLoggedIn;
--- views/middleware/authorization.js
+++
@@ -1,7 +0,0 @@
-const dashBoardLoader = (req, res) => {
-  req.session.userInfo.type == "Admin"
-    ? res.render("user/admin-dashboard")
-    : res.render("user/user-dashboard");
-};
-
-module.exports = dashBoardLoader;
